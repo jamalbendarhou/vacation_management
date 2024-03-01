c4353bf79919f911e0f4a750b6f86afb071690d6
azerty456 <bendarhoujamal@gmail.com>
2024-02-29 22:42:20 -0500

    finition des links du tableau de bord

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,25 @@
 import Card from "@components/dashboard/Card";
 
-
 export default function Home() {
   return (
     <>
-        <main className="ml-60 pt-16 max-h-screen overflow-auto ">
-          <div className="px-6 py-8">
-            <div className="max-w-4xl mx-auto">
-              <div className="bg-white rounded-3xl p-8 mb-5 border-gray-800 border-1 shadow-md">
-                <h1 className="text-xl font-bold mb-10">Vous pouvez gerer tous vos employes ici</h1>
+      <main className="ml-60 pt-16 max-h-screen overflow-auto ">
+        <div className="px-6 py-8 flex justify-center items-center">
+          <div className="max-w-4xl">
+            <div className="bg-white rounded-3xl p-8 mb-5 border-gray-800 border-1 shadow-md">
+              <h1 className="text-xl font-bold mb-10 text-center">Vous pouvez gérer tous vos employés ici</h1>
 
+              <hr className="my-10"/>
 
-                <hr className="my-10"/>
-
-                <div className="grid grid-cols-2 gap-5">
-                      <Card />
-                      <Card />
-                      <Card />
-                      <Card />
-                </div>
+              <div className="grid grid-cols-2 gap-5">
+                <Card title="Gérer les employés" linkTo="/employees" />
+                <Card title="Gérer les vacances" linkTo="/vacations" />
+                  <Card title="Gérer les types de vacances" linkTo="/vacation-types" />
               </div>
             </div>
           </div>
-        </main>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/dashboard/Card.tsx
+++ src/components/dashboard/Card.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 
+type Props = {
+  title: string,
+  linkTo: string
+}
 
-export default function Card() {
+export default function Card({ title, linkTo }: Props) {
   return (
-    <div className="p-4 bg-white border rounded-xl text-gray-800 space-y-2 ">
-        <Link href="" className="font-bold hover:text-yellow-800 hover:underline">******************</Link>
-        <div className="text-sm text-gray-600">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="text-gray-800 inline align-middle mr-1" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-        </svg>*******************
-        </div>
+    <div className="p-4 bg-white border rounded-xl text-gray-800 space-y-2">
+      <Link href={linkTo}>
+        <div className="font-bold hover:text-yellow-800 hover:underline">{title}</div>
+      </Link>
     </div>
   )
 }
